01955f749295fe25e1572cc5e4c8b60c125b6850
Andrew Philip John <114685065+AndrewPJ2002@users.noreply.github.com>
2025-03-17 19:49:50 -0400

    fixed routing for login.html (<a href="{{ url_for('views.login') }}">Login</a>)

--- views.py
+++ views.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, render_template
 
-views = Blueprint(__name__, "views")
+views = Blueprint("views", __name__)
 
 @views.route("/")
 def home():
-    return render_template("./index.html")
+    return render_template("index.html")
+
+# adding a route for login page
+@views.route("/login")
+def login():
+    return render_template("login.html")  
