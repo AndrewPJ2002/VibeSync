8ea9056554d5e75d6f78ffd13e8e322711922e6d
Andrew Philip John <114685065+AndrewPJ2002@users.noreply.github.com>
2025-03-17 21:08:27 -0400

    added models.py and set up databse in supabase

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from views import views
+#from sqlalchemy import text
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.register_blueprint(views)
-    
+
+#supabase connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:TheCodeAlwaysWorks1234@db.lmkoozytmtjowzvwdafm.supabase.co:5432/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False   
+
+db = SQLAlchemy(app) 
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
---
+++ models.py
@@ -0,0 +1,22 @@
+from app import db
+from flask_bcrypt import Bcrypt
+
+# this is to encrypt passwords
+bcrypt = Bcrypt()
+
+# define a user table
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+
+    # hashes password and then decodes it into a string for storage
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    #checks the password with hashed password and then returns true or false
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+    def __repr__(self):
+        return f"<User {self.username}>"
