d28669f17bbf8d4ae707945287e2212d60c720df
Andrew Philip John <114685065+AndrewPJ2002@users.noreply.github.com>
2025-03-18 12:52:41 -0400

    added login logic in views.py

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template #for modular routing and rendering HTML pages
+from flask import Blueprint, render_template, request, redirect, url_for, flash #for modular routing and rendering HTML pages
 
 #creates a blueprint called views
 views = Blueprint("views", __name__)
@@ -8,6 +8,17 @@ def home():
     return render_template("index.html")
 
 # adding a route for login page
-@views.route("/login", methods = ["POST"])
+@views.route("/login", methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user and user.check_password(password):
+            flash("Login successful!", "success")
+            return redirect(url_for("views.home"))
+        else:
+            flash("Invalid credentials, try again.", "danger")
+
     return render_template("login.html")  
